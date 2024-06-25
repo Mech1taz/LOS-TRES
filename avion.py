7ac9ca04572758703f6177d8d8c2a81bd41755ad
Phill721 <germanx9514@gmail.com>
2024-06-25 09:21:48 -0400

    agregado system cls

--- avion.py
+++ avion.py
@@ -1,3 +1,5 @@
+from os import system
+system("cls")
 print("tengo un noctulo")
 print(" un murcielago chico que vive en el desvan de mi casa, no nos vemos nuca, los dos trabajamos de noche")
 print("a")
