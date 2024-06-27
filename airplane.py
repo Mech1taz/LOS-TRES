e95e0d8dd57b3ae3f90f773fc01cc28cdcc0e90b
CETECOM <cetecom_conce@duoc.cl>
2024-06-27 11:29:50 -0400

    agregado funcion

--- airplane.py
+++ airplane.py
@@ -1,7 +1,22 @@
 # ¡¡¡¡Aqui pondremos TODAS LAS FUNCIONES!!!!
 # PARA AGREGAR FUNCIONES A ARCHIVO PRINCIPAL ---> airplane.funcion(argumento)
 from os import system
+asientos=[]
+pasajeros=[]
+persona=[]
+# asiento comun $60.000
+# asiento espacio piernas $80.000
+# asiento preferente $100.000
 def clear():
     system("cls")
+
 def pause():
     input("Presione enter para continuar...")
+
+
+def compra_pasajes(opcion):
+    print(""" Seleccione el asiento a reservar:
+        1.- Asiento común: $60.000
+        2.- Espacio adicional para piernas: $80.000
+        3.- Preferente: $100.000             """)
+    match opcion:
--- avion.py
+++ avion.py
@@ -1,31 +1,33 @@
 from os import system
 system("cls")
 import airplane
-while True:
-    print("""
-    ¡Bienvenidos a Lemon airlines!
-    
-    1- Comprar pasajes
-    2- Mostrar ubicaciones disponibles
-    3- Ver listado de pasajeros
-    4- Buscar pasajero
-    5- Imprimir lista de pasajeros
-    0- Salir""")
-    op=input("Elige una opcion: ")
-    match op:
-        case "1":
-            pass
-        case "2":
-            pass
-        case "3":
-            pass
-        case "4":
-            pass
-        case "5":
-            pass
-        case "0":
-            import sys
-            sys.exit()
-        case other:
-            print("¡Error! Opcion no valida!")
-            airplane.pause()
+print("""
+¡Bienvenidos a Lemon airlines!
+
+1- Comprar pasajes
+2- Mostrar ubicaciones disponibles
+3- Ver listado de pasajeros
+4- Buscar pasajero
+5- Imprimir lista de pasajeros
+0- Salir""")
+
+op=input("Elige una opcion: ")
+match op:
+    case "1":
+        print("BIENVENIDO A LEMON AIRLINES ▀-▀")
+        
+        pass
+    case "2":
+        pass
+    case "3":
+        pass
+    case "4":
+        pass
+    case "5":
+        pass
+    case "0":
+        import sys
+        sys.exit()
+    case other:
+        print("¡Error! Opcion no valida!")
+        airplane.pause()
